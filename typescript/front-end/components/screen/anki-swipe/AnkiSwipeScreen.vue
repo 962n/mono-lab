3cd27568971036d19543b67ed5bd277b16b15644
962n <tak.k1u2r2o4@gmail.com>
2022-07-12 22:56:03 +0900

    setup vue-tinder

--- typescript/front-end/components/screen/anki-swipe/AnkiSwipeScreen.vue
+++ typescript/front-end/components/screen/anki-swipe/AnkiSwipeScreen.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <p>Sorry... This screen is under construction.</p>
+    <client-only>
+      <vue-tinder class="h-80 w-80" key-name="id" :queue.sync="list" :offset-y="10" @submit="onSubmit">
+        <template slot-scope="scope">
+          <div class="h-80 w-80">
+            <h1>HOGEHOGE</h1>
+          </div>
+        </template>
+      </vue-tinder>
+    </client-only>
   </div>
 </template>
 
@@ -9,17 +18,25 @@ import {
   defineComponent,
 } from '@vue/composition-api'
 import {AnkiSwipePresenter} from "~/core/presentation/screen/anki-swipe/anki-swipe";
+import HeaderPublicScreen from "~/components/screen/header-public/HeaderPublicScreen.vue";
 
 export default defineComponent({
+  components: {HeaderPublicScreen},
   props: {
     presenter: Object as () => AnkiSwipePresenter,
   },
   setup(props, context) {
     const p = props.presenter
+    const list = [{id: 1}, {id: 2}, {id: 3}, {id: 4}]
     return {
-      p
+      p,
+      list
     }
   },
-  methods: {}
+  methods: {
+    onSubmit() {
+
+    },
+  }
 })
 </script>
--- typescript/front-end/nuxt.config.js
+++ typescript/front-end/nuxt.config.js
@@ -19,6 +19,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/tailwind.css',
+    '@/assets/css/transition.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -26,6 +27,7 @@ export default {
     '@/plugins/composition-api',
     '@/plugins/vue-tailwind',
     '@/plugins/vue-infinite-loading',
+    '@/plugins/vue-tinder',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
